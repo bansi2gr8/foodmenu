01e7184b4cb564610b9f7ce286227a3d096e3805
Bansi <120380856+bansi2gr8@users.noreply.github.com>
2023-01-19 17:34:15 +0530

    Add files via upload

---
+++ foodmenu.js
@@ -0,0 +1,163 @@
+let a = require("readline-sync");
+day = a.question("Enter your day ");
+if (day == "monday") {
+    console.log("breakfast");
+    console.log("lunch");
+    console.log("dinner");
+    meals = a.question("Chioce any meals ");
+    if (meals == "breakfast") {
+        console.log("poha");
+        console.log("time:-8:00pm");
+    }
+    else if (meals == "lunch") {
+        console.log("raja chawal");
+        console.log("time:-1:00am");
+    }
+    else if (meals == "dinner") {
+        console.log("roti sabji");
+        console.log("time:-8:00pm");
+    }
+    else{
+        console.log("nothing")
+    }
+
+}
+
+else if (day == "tuesday") {
+    console.log("breakfast");
+    console.log("lunch");
+    console.log("dinner");
+    meals = a.question("Chioce any meals ");
+    if (meals == "breakfast") {
+        console.log("dahi and paratha");
+        console.log("time:-8:00pm");
+    }
+    else if (meals == "lunch") {
+        console.log("moosar daal and chawal");
+        console.log("time:-1:00am");
+    }
+    else if (meals == "dinner") {
+        console.log("chhole and roti");
+        console.log("time:-8:00pm");
+    }
+    else{
+        console.log("nothing")
+    }
+
+}
+
+
+else if (day == "wednesday") {
+    console.log("breakfast");
+    console.log("lunch");
+    console.log("dinner");
+    meals = a.question("Chioce any meals ");
+    if (meals == "breakfast") {
+        console.log("bread and milk");
+        console.log("time:-8:00pm");
+    }
+    else if (meals == "lunch") {
+        console.log("pizza");
+        console.log("time:-1:00am");
+    }
+    else if (meals == "dinner") {
+        console.log("chicken and roti");
+        console.log("time:-8:00pm");
+    }
+    else{
+        console.log("nothing")
+    }
+}
+
+
+else if (day == "thursday") {
+    console.log("breakfast");
+    console.log("lunch");
+    console.log("dinner");
+    meals = a.question("Chioce any meals ");
+    if (meals == "breakfast") {
+        console.log("pasta");
+        console.log("time:-8:00pm");
+    }
+    else if (meals == "lunch") {
+        console.log("arhar ki daal or chawal");
+        console.log("time:-1:00am");
+    }
+    else if (meals == "dinner") {
+        console.log("eagg ki sabji or roti");
+        console.log("time:-8:00pm");
+    }
+    else{
+        console.log("nothing")
+    }
+}
+
+
+else if (day == "friday") {
+    console.log("breakfast");
+    console.log("lunch");
+    console.log("dinner");
+    meals = a.question("Chioce any meals ");
+    if (meals == "breakfast") {
+        console.log("maagi");
+        console.log("time:-8:00pm");
+    }
+    else if (meals == "lunch") {
+        console.log("chicken or chawal");
+        console.log("time:-1:00am");
+    }
+    else if (meals == "dinner") {
+        console.log("loo & Gobi ki sabji or roti");
+        console.log("time:-8:00pm");
+    }
+    else{
+        console.log("nothing")
+    }
+}
+
+else if (day == "saturday") {
+    console.log("breakfast");
+    console.log("lunch");
+    console.log("dinner");
+    meals = a.question("Chioce any meals ");
+    if (meals == "breakfast") {
+        console.log("fired rice");
+        console.log("time:-8:00pm");
+    }
+    else if (meals == "lunch") {
+        console.log("channa ki daal or chawal");
+        console.log("time:-1:00am");
+    }
+    else if (meals == "dinner") {
+        console.log("mix vegetable sabji or roti");
+        console.log("time:-8:00pm");
+    }
+    else{
+        console.log("nothing")
+    }
+}
+
+else if (day == "sunday") {
+    console.log("breakfast");
+    console.log("lunch");
+    console.log("dinner");
+    meals = a.question("Chioce any meals ");
+    if (meals == "breakfast") {
+        console.log("sewaiya");
+        console.log("time:-8:00pm");
+    }
+    else if (meals == "lunch") {
+        console.log("sea food");
+        console.log("time:-1:00am");
+    }
+    else if (meals == "dinner") {
+        console.log("egg bhujiya or roti");
+        console.log("time:-8:00pm");
+    }
+    else{
+        console.log("nothing")
+    }
+}
+else{
+    console.log("nothing")
+}
